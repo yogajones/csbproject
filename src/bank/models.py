939ac784fe819c7d341928bbeea590defca966d4
yogajones <95644662+yogajones@users.noreply.github.com>
2024-03-28 12:52:38 +0200

    feat: update index page

--- src/bank/models.py
+++ src/bank/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    iban = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.IntegerField()
